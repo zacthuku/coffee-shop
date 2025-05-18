d2bec665b53118ae204a66751def71789e4f6916
Zacharia Ndinguri <zacthuku@gmail.com>
2025-05-18 10:41:36 +0300

    üpdated order

---
+++ order.py
@@ -0,0 +1,13 @@
+class Order:
+    all=[]
+    def __init__(self, customer, coffee, price):
+        self.customer=customer
+        self.coffee=coffee
+        self.price=price
+        Order.all.append(self)
+    
+    @property
+    def customer (self):
+        return self._customer 
+    @customer.setter
+    def    
